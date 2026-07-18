f267d41c281e1c483d947b20ba9af9ea50bf005d
BFA <82632764+borjamoskv@users.noreply.github.com>
2026-07-18 07:40:59 +0200

    feat(C5-REAL): Prolog FO unification (Martelli-Montanari) + occurs_check in robinson_resolution.pl

--- axioms/robinson_resolution.pl
+++ axioms/robinson_resolution.pl
@@ -1,6 +1,7 @@
 % Claim: [ROBINSON-RESOLUTION-PROLOG] Resolver de Cláusulas por Resolución de Robinson
 % Proof:
 %   Base: "Principio de Resolución de J. Alan Robinson (1965)"
+%         "Unification Algorithm de Martelli-Montanari (1982)"
 %   Confidence: C5-REAL
 
 % Representación de cláusulas como listas de literales.
@@ -35,3 +36,65 @@ probar_inconsistencia(Clausulas, Clausulas) :-
 probar_inconsistencia(Clausulas, HistorialCompleto) :-
     resolucion_paso(Clausulas, NuevoResolvente),
     probar_inconsistencia([NuevoResolvente|Clausulas], HistorialCompleto).
+
+% --- FIRST-ORDER LOGIC EXPANSION ---
+
+% occurs_check(Var, Term): true if Var occurs in Term
+occurs_check(Var, Term) :-
+    var(Term), !, Var == Term.
+occurs_check(Var, Term) :-
+    compound(Term),
+    Term =.. [_|Args],
+    member(Arg, Args),
+    occurs_check(Var, Arg), !.
+
+% unify(T1, T2): Martelli-Montanari Unification with Occurs Check
+unify(T1, T2) :-
+    var(T1), var(T2), T1 == T2, !.
+unify(T1, T2) :-
+    var(T1), !,
+    \+ occurs_check(T1, T2),
+    T1 = T2.
+unify(T1, T2) :-
+    var(T2), !,
+    \+ occurs_check(T2, T1),
+    T2 = T1.
+unify(T1, T2) :-
+    atomic(T1), atomic(T2), !, T1 = T2.
+unify(T1, T2) :-
+    compound(T1), compound(T2),
+    T1 =.. [F|Args1], T2 =.. [F|Args2],
+    length(Args1, L), length(Args2, L),
+    unify_args(Args1, Args2).
+
+unify_args([], []).
+unify_args([H1|T1], [H2|T2]) :-
+    unify(H1, H2),
+    unify_args(T1, T2).
+
+% contrarios_fo(L1, L2): FO complementary literals
+contrarios_fo(L1, neg(L2)) :- unify(L1, L2).
+contrarios_fo(neg(L1), L2) :- unify(L1, L2).
+
+% resolve_fo(C1, C2, Resolvente): First-order resolution
+resolve_fo(C1, C2, Resolvente) :-
+    copy_term(C1-C2, C1Copy-C2Copy),
+    select(L1, C1Copy, Resto1),
+    select(L2, C2Copy, Resto2),
+    contrarios_fo(L1, L2),
+    append(Resto1, Resto2, RawResolvente),
+    sort(RawResolvente, Resolvente).
+
+:- if(false).
+% Test Harness for First-Order Resolution
+test_fo_resolution :-
+    % C1: P(x) | Q(f(x))
+    % C2: ~P(a) | R(y)
+    % Expected Resolvent: Q(f(a)) | R(y)
+    C1 = [p(X), q(f(X))],
+    C2 = [neg(p(a)), r(Y)],
+    resolve_fo(C1, C2, Res),
+    write('C1: '), write(C1), nl,
+    write('C2: '), write(C2), nl,
+    write('Resolvent: '), write(Res), nl.
+:- endif.
